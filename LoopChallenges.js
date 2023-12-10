8406c2476194a033d717b2a578bd35aa304d77f6
SimonAllenOliver <simonallenoliver@gmail.com>
2023-12-09 19:17:16 -0600

    started loop challenges

---
+++ LoopChallenges.js
@@ -0,0 +1,15 @@
+
+// print odds 1-20
+for(var i = 0; i <21; i++){
+    if(i%2 !==0){
+        console.log(i)
+    }
+}
+
+// print multiples of 3 descending from 100
+for(var i = 100; i >0; i--){
+    if(i%3 ==0){
+        console.log(i)
+    }
+}
+

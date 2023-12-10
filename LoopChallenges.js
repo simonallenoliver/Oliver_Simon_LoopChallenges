a491e8747b33dc47265db5ac09506a1eb99a0535
SimonAllenOliver <simonallenoliver@gmail.com>
2023-12-09 20:14:11 -0600

    finish

--- LoopChallenges.js
+++ LoopChallenges.js
@@ -1,15 +1,38 @@
 
 // print odds 1-20
-for(var i = 0; i <21; i++){
-    if(i%2 !==0){
-        console.log(i)
-    }
-}
+// for(var i = 0; i <21; i++){
+//     if(i%2 !==0){
+//         console.log(i)
+//     }
+// }
 
 // print multiples of 3 descending from 100
-for(var i = 100; i >0; i--){
-    if(i%3 ==0){
-        console.log(i)
-    }
+// for(var i = 100; i >0; i--){
+//     if(i%3 ==0){
+//         console.log(i)
+//     }
+// }
+
+
+// print sequence
+// var array1 = [4, 2.5, 1, -0.5, -2, -3.5]
+// for(var i = 0; i  <6; i++){
+//     console.log(array1[i])
+//     }
+
+
+//sigma
+var sum1 = 0
+for(var i = 1; i<101; i++){
+    sum1 = sum1 +i
 }
+console.log(sum1)
+
+//factorial
+
+var product = 1
+for(var i = 1; i<13; i++){
+    product = product * i
+}
+console.log(product)
 
